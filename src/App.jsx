bb2b844f74fc51209aa28c4f985655db8f9a13fb
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-20 11:09:42 +0100

    Added info to each hero and a button to hide

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,20 @@ const App = () => {
 }
 
 const HeroCard = (props) => {
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <p>HERO: {props.heroObj.hero}</p>
+
+      {show && (
       <div>
         <p>INFO: {props.heroObj.info}</p>
         <p>VILLAIN: {props.heroObj.villain}</p>
       </div>
+      )}
+
+      <button onClick={() => setShow(!show)}>SHOW INFO</button>
     </>
   );
 }
