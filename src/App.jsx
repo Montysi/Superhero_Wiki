22a2ed34b08ad4fd3ef785f2f143bf430a03b203
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-20 13:36:15 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,47 @@ import { allHeroes } from './HeroData'
 import './App.css'
 
 const App = () => {
+  const [favourites, setFavourites] = useState([])
 
-  
+  const handleAddToFaves = (hero) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.push(hero);
+    setFavourites(newFaveArr);
+  };
+
+  const handleRemove = (index) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.splice(index, 1);
+    setFavourites(newFaveArr);
+  }
 
   return (
     <div>
-      <h1>My Superhero Wiki</h1>
 
-      {
-        allHeroes.map((heroInfo, index) => {
-          return <HeroCard key={index} heroObj={heroInfo}/>
-        })
-      }
+      <h1>My Superhero Wiki</h1>
+      <div>
+        <h3>My Favourite Heroes</h3>
+        {favourites.map((faveHero, index) => {
+          return <Favourites key={index} faveHeroData={faveHero} removeFave={() => handleRemove(index)} />;
+        })}
+      </div>
+
+      <div>
+        <h3>All Heroes</h3>
+        {allHeroes.map((heroInfo, index) => {
+          return <HeroCard key={index} heroObj={heroInfo} faveFunc={handleAddToFaves} />;
+        })}
+      </div>
+
+    </div>
+  );
+};
+
+const Favourites = ({faveHeroData, removeFave}) => {
+  return (
+    <div>
+     <p>{faveHeroData.hero}</p>
+     <button onCLick={removeFave}>X</button>
     </div>
   )
 }
@@ -27,13 +56,14 @@ const HeroCard = (props) => {
       <p>HERO: {props.heroObj.hero}</p>
 
       {show && (
-      <div>
-        <p>INFO: {props.heroObj.info}</p>
-        <p>VILLAIN: {props.heroObj.villain}</p>
-      </div>
+        <div>
+          <p>INFO: {props.heroObj.info}</p>
+          <p>VILLAIN: {props.heroObj.villain}</p>
+        </div>
       )}
 
-      <button onClick={() => setShow(!show)}>SHOW INFO</button>
+      <button onClick={() => setShow(!show)}>{show ? "HIDE INFO" : "SHOW INFO"}</button>
+      <button onClick={() => props.faveFunc(props.heroObj)}>Add to Faves</button>
     </>
   );
 }
