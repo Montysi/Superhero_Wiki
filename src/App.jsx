97e513356ea50cbc6a3a3eb148ff97b0929c0ab9
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-20 10:57:29 +0100

    added HeroCard list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import { allHeroes } from './HeroData'
 import './App.css'
 
 const App = () => {
 
+  
+
   return (
     <div>
       <h1>My Superhero Wiki</h1>
+
+      {
+        allHeroes.map((heroInfo, index) => {
+          return <HeroCard key={index} heroObj={heroInfo}/>
+        })
+      }
     </div>
   )
+}
+
+const HeroCard = (props) => {
+  return (
+    <>
+      <p>HERO: {props.heroObj.hero}</p>
+      <div>
+        <p>INFO: {props.heroObj.info}</p>
+        <p>VILLAIN: {props.heroObj.villain}</p>
+      </div>
+    </>
+  );
 }
 
 export default App
